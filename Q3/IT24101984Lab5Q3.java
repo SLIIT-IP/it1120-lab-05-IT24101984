f116f1ee6046d51f5ea62b9c58a5160faec945e4
Vithanage T.A <it24101984@my.sliit.lk>
2024-08-22 20:09:24 +0530

    Add IT24101984Lab5Q3.java to Q3

---
+++ Q3/IT24101984Lab5Q3.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+public class IT24101984Lab5Q3  {
+public static void main(String[] args)  {
+
+Scanner input = new Scanner(System.in);
+ 
+//Declare Variables
+  int stDate,enDate,dayRes;
+  double totAmount;
+
+//Constant values
+  final double CHARGE = 48000;
+  final int DAYS3_4 = 90;
+  final int DAYS_5 = 80;
+
+//Get user input
+System.out.print("Enter start Date : ");
+stDate = input.nextInt();
+
+System.out.print("Enter the end Date : ");
+enDate = input.nextInt();
+
+//Validate the inputs
+
+  if (stDate < 1 || stDate > 31 || enDate < 1 || enDate > 31)
+   {
+     System.out.print("Days must be between 1 and 31");
+     return;
+   }
+  
+
+  if (stDate >= enDate)
+   {
+     System.out.print("Start date must be less than end Date");
+     return;
+   }
+
+
+ //find no of days resered
+ dayRes = enDate - stDate;  
+ 
+
+ //Calculate the total amount to be paid
+
+  if (dayRes < 3)
+    {
+      totAmount = CHARGE * dayRes;
+    }
+  else if (dayRes <= 4)
+     {
+       totAmount = CHARGE * dayRes * DAYS3_4 / 100;
+     }
+  else
+    {
+      totAmount = CHARGE * dayRes * DAYS_5 / 100;
+    }
+  
+
+   System.out.print("\n");  //Break a line
+
+  //Display the final output
+   System.out.println("Room Charge per Day is : " + "RS." + CHARGE);
+   System.out.println("Number of Days Reserved : " + dayRes);
+   System.out.print("Total Amount to be paid : " + totAmount);
+
+  }
+}
